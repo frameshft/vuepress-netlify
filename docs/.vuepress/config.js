36608d044a7d54b9b7a7e76d6022fa09cef04e4e
frameshft <adiletkadyrov@gmail.com>
2019-04-16 21:18:13 +0600

    Remove unused code

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,3 @@
-const dirTree = require('directory-tree');
-const path = require('path');
-
-var projets = [];
-dirTree(path.join(__dirname, '../parent-1'), {extensions:/\.md/}, (item, PATH) => projets.push(item));
-projets = projets.map(children => {
-    return path.parse(children.name).name  !== 'README' ? path.join.apply(null, children.path.split(path.sep).slice(7)) : path.join.apply(null, children.path.split(path.sep).slice(7)).replace('README.md', '');
-});
-
-console.log(projets)
 module.exports = {
 
   title: 'shipDocs',
